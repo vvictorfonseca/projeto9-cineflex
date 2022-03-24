40d4a3ca98fac5bd3a1706e15254328584aea432
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-03-24 01:03:28 -0300

    Versão 2.0

--- projeto9-cineflex/src/Componentes/Filme.js
+++ projeto9-cineflex/src/Componentes/Filme.js
@@ -1,5 +1,36 @@
-export default function Filme () {
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
+export default function Filme() {
+
+    const [imagens, setImagens] = useState([])
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        promise.then(response => {
+            const { data } = response;
+            console.log("vim da API", data)
+            setImagens(data)
+        })
+    }, [])
+
     return (
-        <h2>Selecione o filme</h2>
+        <>
+            <h2>Selecione o filme</h2>
+            <session className="tela-filme">
+
+                {imagens.map((imagem) => {
+                    const { posterURL, id } = imagem;
+                    return <div key={id} className="tela-filme-moldura">
+                        <Link to={`/sessoes/${id}`}>
+                        <img className="tela-filme-foto" src={posterURL}></img>
+                        </Link>
+                    </div>
+                })}
+            </session>
+        </>
     )
 }
+
