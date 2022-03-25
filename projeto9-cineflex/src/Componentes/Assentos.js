ad9ff48d590352c3e4c6aed643524808aa90ee72
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-03-25 14:16:23 -0300

    Versão 4.0

--- projeto9-cineflex/src/Componentes/Assentos.js
+++ projeto9-cineflex/src/Componentes/Assentos.js
@@ -1,5 +1,100 @@
-export default function Assentos () {
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function Assentos() {
+
+    const { idSessao } = useParams()
+    const [seats, setSeats] = useState([])
+    const [footer, setFooter] = useState([])
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`);
+        promise.then(response => {
+            console.log("vim da API", response.data)
+            setSeats(response.data.seats)
+            setFooter(response.data.movie)
+            setData(response.data.day)
+        })
+    }, [])
+
+    const[Selecionado,setSelecionado] = useState(false)
+
+    const [addAssento, setAddAssento] = useState([])
+    const adicionarAssento = (resultado) => {setAddAssento([...addAssento], addAssento.push(resultado))}
+
+    console.log(addAssento)
+    console.log(Selecionado)
+
+    function add () {
+        setSelecionado(true)
+        adicionarAssento(seats.name)
+    }
+
     return (
-        <h2>Selecione o(s) assento(s)</h2>
+
+        <>
+            <h2>Selecione o(s) assento(s)</h2>
+
+            <session className="tela-sessoes">
+
+                <div className="caixa-assentos">
+                    {seats.map((seat) => {
+                        const {name, isAvailable} = seat
+                        return isAvailable ?
+                        <div className="assentos-disponiveis">
+                         <p className="assentos-p">{name}</p>
+                        </div>
+                        :
+                    <div className="assentos-indisponiveis">
+                         <p className="assentos-p">{name}</p>
+                        </div>
+                        
+                    })}
+                </div>
+
+                <div className="opcoes-assentos">
+                    <div className="opcao-assentos">
+                        <div className="circulo-assento"></div>
+                        <p>Selecionado</p>
+                    </div>
+
+                    <div className="opcao-assentos">
+                        <div className="circulo-assento2"></div>
+                        <p>Disponível</p>
+                    </div>
+
+                    <div className="opcao-assentos">
+                        <div className="circulo-assento3"></div>
+                        <p>Indisponível</p>
+                    </div>
+                </div>
+
+                <div className="inputs">
+                    <p>Nome do comprador:</p>
+                    <input type="text" placeholder="Digite seu nome..."/>
+                    <p>CPF do comprador:</p>
+                    <input type="text" placeholder="Digite seu CPF..."/>
+
+                    <button>Reservar assento(s)</button>
+                </div>
+
+                
+                <div className="fundo">
+                    <div className="moldura-img-fundo">
+                        <img className="img-fundo" src={footer.posterURL}></img>
+                    </div>
+                    <div>
+                    <p>{footer.title}</p>
+                    <span>{data.weekday} - {data.date}</span>
+                    </div>
+                </div>
+
+            </session>
+        </>
+
     )
 }
--- projeto9-cineflex/src/Componentes/Horario.js
+++ projeto9-cineflex/src/Componentes/Horario.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -7,6 +7,7 @@ export default function Horario() {
 
     const { idFilme } = useParams()
     const [filme, setFilme] = useState([])
+    const [footer, setFooter] = useState([])
 
     useEffect(() => {
 
@@ -14,6 +15,8 @@ export default function Horario() {
         promise.then(response => {
             console.log("vim da API", response.data.days)
             setFilme(response.data.days)
+            setFooter(response.data)
+
         })
     }, [])
 
@@ -24,19 +27,28 @@ export default function Horario() {
             {filme.map((dia) => {
                 const { weekday, date, showtimes } = dia;
 
-            return <session className="tela-sessoes">
+                return <session className="tela-sessoes">
                     <div className="dia-data">
                         <span>{weekday} - {date} </span>
                     </div>
 
                     <div className="boxes">
-
                         {showtimes.map((hora) => {
-                            const { name } = hora
-
-                            return <div className="box"><p className="horarios">{name}</p></div>
+                            const { name, id } = hora
+                            
+                            return  <Link to={`/assentos/${id}`}>
+                            <div className="box"><p className="horarios">{name}</p></div>
+                            </Link>
                         })}
                     </div>
+
+                    <div className="fundo">
+                        <div className="moldura-img-fundo">
+                            <img className="img-fundo" src={footer.posterURL}></img>
+                        </div>
+                        <p>{footer.title}</p>
+                    </div>
+
                 </session>
             })}
         </>
